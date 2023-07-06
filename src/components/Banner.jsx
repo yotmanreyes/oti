573ab39df6e4055c1cb3ff2705c53fea33ff0f9f
yotmanreyes <yotman.reyes@gmail.com>
2023-07-06 15:41:36 -0400

    add components

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Banner(){
+    return (
+        <div className="banner">
+            <h2 className="text-3xl font-bold underline">Imagina sin limites, nosotros lo hacemos realidad</h2>
+            <p>Oti Valencia, C.A. es una empresa dedicada al sector construcción, 
+                especializada en las disciplina de obras civiles, instalaciones eléctricas, 
+                instalaciones sanitarias y sistemas contra incendios.</p>
+        </div>
+    );
+}
