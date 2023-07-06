135aa79fc113eb9bf077597ba85109338a84afe5
yotmanreyes <yotman.reyes@gmail.com>
2023-07-06 15:42:18 -0400

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Menu from './components/Menu';
+import Banner from './components/Banner';
+import Services from './sections/Services';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,6 +12,8 @@ function App() {
   return (
     <>
       <Menu></Menu>
+      <Banner></Banner>
+      <Services></Services>
     </>
   )
 }
